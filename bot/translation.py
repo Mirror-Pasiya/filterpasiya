5303a50c1ac0d7fa01920cc589ddb55e56c93800
Mirror-Pasiya <98305239+Mirror-Pasiya@users.noreply.github.com>
2022-06-06 10:04:52 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -7,10 +7,10 @@ class Translation(object):
     START_TEXT = """<b>Hey {}!!</b>
 <i>Am Just A Advance Auto Filter Bot....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+Just Add Me To Your Group And Channel And Connect Them And See My Pevers 😏
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@TG_NSns_999
 </i>"""    
     
     HELP_TEXT = """
