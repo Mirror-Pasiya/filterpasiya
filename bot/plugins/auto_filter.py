61d075e8be1c227bf95d102ee4068f30a54ded56
Mirror-Pasiya <98305239+Mirror-Pasiya@users.noreply.github.com>
2022-06-06 09:40:25 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -203,7 +203,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"Found {(len_results)} Results For Your Query: <code>{query}</code>",
+                text=f"🤗මෙන්න {(len_results)} ඔයා හොයපු: <code>{query}</code>👇",
                 reply_markup=reply_markup,
                 parse_mode=enums.ParseMode.HTML,
                 reply_to_message_id=update.id
